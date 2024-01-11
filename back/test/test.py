571b8bca0c2aa049b05f53edcc6e5574c93cadc8
valtzman <xmodgamecenterhon@gmail.com>
2024-01-11 23:55:54 +0600

    work with json file

---
+++ back/test/test.py
@@ -0,0 +1,20 @@
+import json 
+
+with open("example.json", "r") as json_file:
+    a = json.load(json_file)
+
+# print(a["Name"])
+# print(a.keys())
+
+# del a["Media"]["OnlyFans"]
+# print(a)
+
+# работа только с питоновским словарем а не с json
+# a["Mass"] = [1, "h", {"a": 1}, [1, 2, 3]]
+# print(a)
+
+a["Mass"] = [1, "h", {"a": 1}, [1, 2, 3]]
+print(json.dumps(a, indent = 2 ))
+
+with open("example.json", "w") as json_file:
+    json.dump(a, json_file, indent = 2)
