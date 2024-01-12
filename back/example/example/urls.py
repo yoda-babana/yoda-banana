b03571b677cbd2d796f5f44c749ac1a0d887fc5d
valtzman <xmodgamecenterhon@gmail.com>
2024-01-12 20:15:29 +0600

    look

---
+++ back/example/example/urls.py
@@ -0,0 +1,20 @@
+# myproject/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('myapp.urls')),
+]
+
+# myapp/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
